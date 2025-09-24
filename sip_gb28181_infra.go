cff38341f81a509ee64489ed95abcd8b858c7954
thinkgo <thinkgo@aliyun.com>
2025-09-24 15:21:54 +0800

    feat: add next sn gen

--- sip_gb28181_infra.go
+++ sip_gb28181_infra.go
@@ -1,1 +1,20 @@
 package sip_gb28181
+
+import (
+	"math/rand/v2"
+	"sync/atomic"
+)
+
+var _sn = atomic.Uint32{}
+
+func init() {
+	_sn.Store(rand.Uint32())
+}
+
+func NextSN() int64 {
+	for {
+		if v := _sn.Add(1); v > 0 {
+			return int64(v)
+		}
+	}
+}
