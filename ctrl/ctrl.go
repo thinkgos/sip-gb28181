095c611422e3ee9c1e55e3381cad2eac0db7c80b
thinkgo <thinkgo@aliyun.com>
2025-10-09 10:16:17 +0800

    chore: fix comment

--- ctrl/ctrl.go
+++ ctrl/ctrl.go
@@ -2,19 +2,21 @@ package ctrl
 
 const DeviceControl_Magic byte = 0xA5
 
-// 组合码1
-// 高4位bit代表的是版本信息, 低4位bit代表的是校验位.
-// 校验位 = (字节1的高是位+字节1的低的位+字节2的高的位)%16
+// CombinedCode1 组合码1, 高4位bit代表的是版本信息, 低4位bit代表的是校验位.
+// 校验位 = (字节1的高4位+字节1的低4位+字节2的高4位)%16
 func CombinedCode1(magic, ver byte) byte {
-	return (magic>>4)&0x0f + magic&0x0f + ver&0x0f
+	checksum := (magic>>4)&0x0f + magic&0x0f + ver&0x0f
+	version := (ver & 0x0f) << 4
+	return version | checksum
 }
 
 // 组合码2
-// 高4位是数据3，低4位是地址的高4位,没有特别指明高是位, 表明与所指定功能无关
+// 高4位是数据3，低4位是地址的高4位, 没有特别指明高4位, 表明与所指定功能无关.
 func CombinedCode2(data3, addr byte) byte {
-	return (data3<<4)&0xf0 + addr&0x0f
+	return (data3&0x0f)<<4 + addr&0x0f
 }
 
+// Checksum 计算校验和, 对所有字节求和后取低8位.
 func Checksum(bs []byte) byte {
 	sum := int(0)
 	for _, v := range bs {
